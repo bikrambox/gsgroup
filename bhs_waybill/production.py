a5a17b8444f03d8246edaec8aa4a6d3ea1b1c3fb
Bikram rongpi <bikramrongpi088@gmail.com>
2025-02-27 15:08:37 +0530

    Update production.py

--- bhs_waybill/production.py
+++ bhs_waybill/production.py
@@ -1,10 +1,25 @@
+# from waitress import serve
+# from app import app
+# import os
+# from dotenv import load_dotenv
+
+# # Load environment variables from .env file
+# load_dotenv()
+
+# if __name__ == '__main__':
+#     host = os.getenv('HOST', '0.0.0.0')
+#     port = int(os.getenv('PORT', 5500))
+    
+#     print(f"Starting BHS Waybill in production mode on {host}:{port}...")
+#     serve(app, host=host, port=port)
+
+
 from waitress import serve
 from app import app
-from waybill_ftp import FTPConnection
+from waybill_ftp import FTPConnection  # Import FTPConnection
 import os
 from dotenv import load_dotenv
 import logging
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
