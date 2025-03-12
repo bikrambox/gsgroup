b1d4cd06886fd289f81db44ddb41e36cfea8142a
Bikram rongpi <bikramrongpi088@gmail.com>
2025-03-12 13:23:21 +0530

    SDDSS

--- frontend/src/views/file_upload.vue
+++ frontend/src/views/file_upload.vue
@@ -1,8 +1,9 @@
 <!-- FileName: frontend\src\views\file_upload.vue -->
 <template>
   <div class="max-w-2xl mx-auto p-6 bg-white">
+    <h2 class="text-5xl font-bold tracking-tight text-gray-600 dark:text-white sm:text-7xl text-center" style="font-family: Helvetica, sans-serif;">BHS logistics</h2>
     <!-- File Input with Drag-and-Drop -->
-    <div class="border-2 border-dashed border-gray-300 rounded-lg p-6 text-center relative mt-4"
+    <div class="border-2 border-dashed border-gray-300 rounded-lg p-6 text-center relative mt-4 dark:bg-gray-800"
       @dragover.prevent="handleDragOver" @dragleave.prevent="handleDragLeave" @drop.prevent="handleDrop"
       :class="{ 'bg-blue-50': isDragging }">
       <input type="file" ref="fileInput" @change="handleFileChange" class="hidden" accept=".json" multiple />
